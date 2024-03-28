9959cbf1edaa582ced1ce21f300a15ae6e99f4d4
Posciutto_32 <75217695+WoralQuaz@users.noreply.github.com>
2024-03-28 01:40:07 +0100

    Update index.ts

--- src/lib/my-custom-plugin/index.ts
+++ src/lib/my-custom-plugin/index.ts
@@ -1,9 +1,12 @@
+
 import {YourGlobalConfig} from './types';
 import {PluginInterface, PluginParams} from '../types/interface';
 
-export const MyCustomPlugin = (
-  globalConfig: YourGlobalConfig
-): PluginInterface => {
+export const NNE = (): PluginInterface => {
+  //
+  const METRICS = ['hardware/training', 'time/training', 'efficency/training', 'power/training',
+                   'hardware/querying', 'time/querying', 'efficency/querying', 'power/querying']; 
+  //
   const metadata = {
     kind: 'execute',
   };
@@ -11,15 +14,36 @@ export const MyCustomPlugin = (
   /**
    * Execute's strategy description here.
    */
-  const execute = async (inputs: PluginParams[]): Promise<PluginParams[]> => {
+  const execute = async (inputs: PluginParams[]): Promise<PluginParams[]> => { //: Promise<PluginParams[]> ??
     return inputs.map(input => {
-      // your logic here
+      // your logic here ??
       globalConfig;
 
-      return input;
+      return {
+        //
+        ... input,
+        'energy-consumed-by-NN': calculateenergy(input) ;
+        //
+      };
     });
   };
+  //
+ const calculateenergy = (input: PluginParams) => {
+    const hardwaretraining= input['hardware/training'];
+    const timetraining = input['time/training'];
+    const efficencytraining = input['efficency/training'];
+    const powertraining= input['power/training'];
+    const hardwarequerying= input['hardware/querying'];
+    const timequerying = input['time/querying'];
+    const efficencyquerying = input['efficency/querying'];
+    const powerquerying= input['Hardware/querying'];
 
+    return (
+     (hardwaretraining * timetraining * efficencytraining * powertraining + 
+      hardwarequerying * timequerying * efficencyquerying * powerquerying)
+    );
+  };
+  //
   return {
     metadata,
     execute,
